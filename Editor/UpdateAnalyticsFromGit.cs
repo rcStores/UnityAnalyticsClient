6592f8297a5aab43e24f446c01d47ccb82f04e12
rc-stores <arsenyivchenko@gmail.com>
2022-06-30 14:58:39 +0700

    Add updating script

---
+++ Editor/UpdateAnalyticsFromGit.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class UpdateAnalyticsFromGit : MonoBehaviour
+{
+    private const string REPOSITORY_PATH = "https://github.com/rcStores/UnityAnalyticsClient.git";
+
+    [MenuItem("Tools/Advant Analytics/Update SDK")]
+    static public void UpdatePackage()
+    {
+        var addRequest = Client.Add(REPOSITORY_PATH);
+        if (addRequest.Status == StatusCode.Failure)
+        {
+            throw new UnityException("Error while updating analytics SDK: " + addRequest.Error.message);
+        }
+        else if (addRequest.Status == StatusCode.Success)
+        {
+            Debug.Log(addRequest.Result.name + " was updated");
+        }
+    }
+}
