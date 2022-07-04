ef64026ec349ff977da586e466a34044041de70b
rc-stores <arsenyivchenko@gmail.com>
2022-07-04 18:23:45 +0700

    Update UpdateAnalyticsFromGit.cs

--- Editor/UpdateAnalyticsFromGit.cs
+++ Editor/UpdateAnalyticsFromGit.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using System.Threading.Tasks;
 
@@ -8,20 +9,36 @@ public class UpdateAnalyticsFromGit : MonoBehaviour
 {
     private const string REPOSITORY_PATH = "https://github.com/rcStores/UnityAnalyticsClient.git";
 
+	static AddRequest s_AddRequest;
+	
     [MenuItem("Tools/Advant Analytics/Update SDK")]
     public static void UpdatePackage()
     {
-        var addRequest = Client.Add(REPOSITORY_PATH);
-
-        if (addRequest.Status == StatusCode.Failure)
-        {
-            throw new UnityException("Error while updating analytics SDK: " + addRequest.Error.message);
-        }
-        else if (addRequest.Status == StatusCode.Success)
-        {
-            Debug.Log(addRequest.Result.name + " was updated");
+        s_AddRequest = Client.Add(REPOSITORY_PATH);
+		EditorApplication.update += PackageRemovalProgress;
+		//EditorApplication.LockReloadAssemblies();
+    }
+	
+	static void PackageRemovalProgress() 
+	{
+		if (s_AddRequest?.IsCompleted) {
+			switch (s_RemRequest.Status) {
+				case StatusCode.Failure:
+					throw new UnityException("Error while updating analytics SDK: " + addRequest.Error.message);
+					break;
+ 
+				case StatusCode.InProgress:
+				break;
+ 
+				case StatusCode.Success:
+					Debug.Log(addRequest.Result.name + " was updated");
+					EditorApplication.update -= PackageRemovalProgress;
+					//EditorApplication.UnlockReloadAssemblies();
+					s_AddRequest = null;
+					break;
         }
     }
+}
 	//public static void Foo()
 	//{}
 }
