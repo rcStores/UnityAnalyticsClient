a2bfd87f9543ddd464440a450c9d08989426bb7b
rc-stores <arsenyivchenko@gmail.com>
2022-07-28 16:59:08 +0700

    Update Backend.cs

--- Runtime/Http/Backend.cs
+++ Runtime/Http/Backend.cs
@@ -74,7 +74,7 @@ namespace Advant.Http
                 throw new Exception(
 					"Http request failure. Response code: " + request.responseCode + 
 					"\nError: " + request.error +
-					"\nDownload handler error (if any): " + request.downloadHandler.error
+					"\nDownload handler error (if any): " + request.downloadHandler.error +
 					"\nUpdload handler data: " + Encoding.UTF8.GetString(req.uploadHandler.data));
             }
             return request.downloadHandler.text;
