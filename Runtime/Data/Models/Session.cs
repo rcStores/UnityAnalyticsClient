98516d4eda6873f92ae4fd75b77a354530731c59
rc-stores <arsenyivchenko@gmail.com>
2022-09-27 12:09:17 +0700

    Update Session.cs

--- Runtime/Data/Models/Session.cs
+++ Runtime/Data/Models/Session.cs
@@ -14,6 +14,7 @@ internal struct Session
 	
 	internal void ToJson(long userId, StringBuilder sb)
 	{
-		sb.Append($"{{\"UserId\":{userId}, \"Area\":\"{Area}\", \"SessionStarts\":\"{SessionStart.ToString("yyyy-MM-ddTHH:mm:ss.fff", CultureInfo.InvariantCulture)}\",\"LastActivity\":\"{LastActivity.ToString("yyyy-MM-ddTHH:mm:ss.fff", CultureInfo.InvariantCulture)}\", \"SessionCount\": {SessionCount}, \"AbMode\":\"{AbMode}\"}}");
+		sb.Append($"{{\"UserId\":{userId}, \"Area\":\"{Area}\", \"SessionStarts\":\"{SessionStart.ToString("yyyy-MM-ddTHH:mm:ss.fff", CultureInfo.InvariantCulture)}\"," 
+			+ "\"LastActivity\":\"{LastActivity.ToString("yyyy-MM-ddTHH:mm:ss.fff", CultureInfo.InvariantCulture)}\", \"SessionId\": \"{SessionId}\", \"AbMode\":\"{AbMode}\"}}");
 	}
 }
