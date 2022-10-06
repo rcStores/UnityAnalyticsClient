64442ebab5cc660f7f7c56a9a9494d2d3239882a
rc-stores <arsenyivchenko@gmail.com>
2022-10-06 20:28:43 +0700

    Update NetworkTimeHolder.cs

--- Runtime/NetworkTimeHolder.cs
+++ Runtime/NetworkTimeHolder.cs
@@ -49,7 +49,7 @@ internal class NetworkTimeHolder
             }
             else
             {
-				_networkInitialTime += _systemInitialTime.AddSeconds((DateTime.UtcNow - currentNetworkTime).TotalSeconds);
+				_networkInitialTime = _networkInitialTime + currentNetworkTime.AddSeconds(-(DateTime.UtcNow - _systemInitialTime).TotalSeconds);
 				Debug.LogWarning("[ADVANAL] network initial time: " + _networkInitialTime);
 				break;
             }
